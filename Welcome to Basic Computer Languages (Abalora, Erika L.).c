6173e3982f3c3e93930c999fb5502d032ea6ebf5
abaloraerika <95512863+abaloraerika@users.noreply.github.com>
2024-05-29 20:00:28 +0800

    Add files via upload

---
+++ Welcome to Basic Computer Languages (Abalora, Erika L.).c
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Rizal Technological University \n";
+    cout << "College of Arts and Sciences \n";
+    cout << "Bachelor of Science in Statistics \n";
+    cout << "Welcome to Basic Computer Languages";
+    return 0;
+}
