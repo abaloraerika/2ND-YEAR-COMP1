cb702d14bd894aa62443579c84592a28eecf923e
abaloraerika <95512863+abaloraerika@users.noreply.github.com>
2024-05-29 22:17:43 +0800

    Add files via upload

---
+++ Even (Abalora, Erika L.).cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // Display even numbers from 1 to 100
+    cout << "The even numbers from 1 to 100 are: \n";
+    for (int i = 2; i <= 100; i += 2) {
+        cout << i << "\n";
+    }
+    return 0;
+}
