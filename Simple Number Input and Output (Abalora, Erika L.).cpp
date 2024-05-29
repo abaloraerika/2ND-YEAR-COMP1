08a622f2846db604863c0efa7243cc90ccb1361d
abaloraerika <95512863+abaloraerika@users.noreply.github.com>
2024-05-29 20:05:36 +0800

    Add files via upload

---
+++ Simple Number Input and Output (Abalora, Erika L.).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float x_number;
+    cout << "Enter a number: ";
+    cin >> x_number;
+    cout << "The number is: " << x_number << endl;
+
+    return 0;
+}
