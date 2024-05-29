49d744c58ed190f186f637bf4bb5adcd151248b2
abaloraerika <95512863+abaloraerika@users.noreply.github.com>
2024-05-29 19:51:15 +0800

    Add files via upload

---
+++ Profile (Abalora, Erika L).cpp
@@ -0,0 +1,22 @@
+// Declare and display variables that will
+// introduce yourself with your name, age,
+// address, phone number, school,
+// and department.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name = "Erika Abalora", address = "Quezon City", school = "RTU", dept = "Statistics";
+    int age = 22, number=6399 ;
+
+    cout << "\t\tProfile\n\n";
+    cout << "Name: " << name << "\n";
+    cout << "Address: " << address << "\n";
+    cout << "Age: " << age << "\n";
+    cout << "Contact Number: " << number << "\n";
+    cout << "School: " << school << "\n";
+    cout << "Department: " << dept << "\n";
+
+    return 0;
+}
