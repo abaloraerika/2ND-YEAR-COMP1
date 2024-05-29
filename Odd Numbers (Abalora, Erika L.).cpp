ffb3a4c43ac2ba225ddfeb18cf0a95c6262807fc
abaloraerika <95512863+abaloraerika@users.noreply.github.com>
2024-05-29 22:05:51 +0800

    Add files via upload

---
+++ Odd Numbers (Abalora, Erika L.).cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Display odd numbers from 1-100
+    int i;
+    
+    for(i = 1; i <= 100; i++)
+    {
+        cout<<i<<"\n";
+    }
+    
+    return 0;
+}
